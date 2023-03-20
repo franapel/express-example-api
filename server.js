2a9474689fbd7776f5fbef934439b86aed1082b9
fperez <fperez@wholemeaning.com>
2023-03-19 23:43:55 -0300

    Agregar endpoints para proyecto de tesis

--- server.js
+++ server.js
@@ -14,11 +14,29 @@ app.get('/', (req, res) => {
   res.send(output)
 })
 
-app.post('/', (req, res) => {
+app.post('/api/activity/session/start', (req, res) => {
   console.log(req.body)
   fs.appendFileSync('output.txt', `\n${JSON.stringify(req.body)}`);
-  res.send('Bye!')
+  res.send('Start!')
 })
+
+app.post('/api/activity/session/end', (req, res) => {
+  console.log(req.body)
+  fs.appendFileSync('output.txt', `\n${JSON.stringify(req.body)}`);
+  res.send('End!')
+})
+
+app.post('/api/activity/session/alive', (req, res) => {
+  console.log(req.body)
+  fs.appendFileSync('output.txt', `\n${JSON.stringify(req.body)}`);
+  res.send('Alive!')
+})
+
+// app.post('/', (req, res) => {
+//   console.log(req.body)
+//   fs.appendFileSync('output.txt', `\n${JSON.stringify(req.body)}`);
+//   res.send('Bye!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
